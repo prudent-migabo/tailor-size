58caac4ed6095d97f71af80b11ae3d9e9ed6c0ef
prudent16 <lyadungamigaboprudent@gmail.com>
2023-11-14 23:38:51 +0200

    feat: firebase caching

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tailor_size/business_logic/business_logic.dart';
@@ -12,6 +13,9 @@ void main() async {
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseFirestore.instance.settings = Settings(
+    persistenceEnabled: true,
   );
   runApp(const MyApp());
 }
